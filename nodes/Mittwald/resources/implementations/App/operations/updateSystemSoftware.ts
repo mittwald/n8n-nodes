a2f19c852b03eac78770eaf46aace28c23df70d0
Daniel <d.haering@mittwald.de>
2026-02-05 14:51:40 +0100

    add update System software versions preparation

--- nodes/Mittwald/resources/implementations/App/operations/updateSystemSoftware.ts
+++ nodes/Mittwald/resources/implementations/App/operations/updateSystemSoftware.ts
@@ -1,25 +1,45 @@
 import { appResource } from '../resource';
-import Z from 'zod';
 import appInstallationProperty from '../../shared/appInstallationProperty';
 import systemSoftwareProperty from '../../shared/systemSoftwareProperty';
+import versionProperty from '../../shared/versionProperty';
 
 export default appResource
 	.addOperation({
-		name: 'Update System Software',
-		action: 'Update System Software',
+		name: 'Update Software Versions',
+		action: 'Update System Software Versions',
 	})
 	.withProperties({
 		appInstallation: appInstallationProperty,
 		systemSoftware: systemSoftwareProperty,
+		version: versionProperty,
 	})
 	.withExecuteFn(async (context) => {
-		const { properties, apiClient } = context;
-		const { appInstallation } = properties;
+		const { systemSoftware } = context.properties;
+		const { apiClient } = context;
 
 		// @ts-ignore
-		console.log(properties.systemSoftware);
+		console.log(systemSoftware);
+		for (const [systemSoftwareId, semverTarget] of Object.entries(systemSoftware ?? {})) {
+			// @ts-ignore
+			console.log(`System Software ID: ${systemSoftwareId}, Target Version: ${semverTarget}`);
+			const matchingVersions = await apiClient.request<
+				Array<{
+					id: string;
+					externalVersion: string;
+				}>
+			>({
+				method: 'GET',
+				path: '/system-softwares/' + systemSoftwareId + '/versions',
+				qs: {
+					versionRange: semverTarget,
+				},
+			});
+			// @ts-ignore
+			console.log(
+				`${semverTarget} Resolved Version ID: ${JSON.stringify(matchingVersions.map((v) => ({ id: v.id, externalVersion: v.externalVersion })))}`,
+			);
+		}
 
-		const appInstallationResponse = null;
 		/*
 		await apiClient.request({
 			method: 'PATCH',
@@ -31,5 +51,5 @@ export default appResource
 			body: {},
 		});
 */
-		return appInstallationResponse;
+		return {};
 	});
